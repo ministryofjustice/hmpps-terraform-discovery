baf36381987f47b275df5c8365f239094ae62b4d
Sandhya1874 <39799075+Sandhya1874@users.noreply.github.com>
2024-08-22 17:20:32 +0100

    Update terraform_discovery.py
    Corrected variable name for elasticache

--- terraform_discovery.py
+++ terraform_discovery.py
@@ -151,7 +151,7 @@ def process_repo(**component):
             # If there are any rds instances in the existing SC data
             if sc_data[0]["attributes"]["elasticache_cluster"]:
               # Find the elasticache cluster SC ID that matches
-              elasticache_id = list(filter(lambda elastichache: elastichache['tf_path'] == elasticache_cluster['__tfmeta']['path'], sc_data[0]["attributes"]["elasticache_cluster"]))[0]['id']
+              elasticache_id = list(filter(lambda elasticache: elasticache['tf_path'] == elasticache_cluster['__tfmeta']['path'], sc_data[0]["attributes"]["elasticache_cluster"]))[0]['id']
               elasticache_cluster.update({'id': elasticache_id})
           except (IndexError,KeyError):
             pass
