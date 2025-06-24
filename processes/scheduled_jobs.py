72512f701501c1676c3a64d78ca79c4f6dadcef0
s99mj <sandhya.gandalwar@justice.gov.uk>
2025-06-24 10:11:47 +0100

    Replaced logging with job_log_handling.py

--- processes/scheduled_jobs.py
+++ processes/scheduled_jobs.py
@@ -1,13 +1,8 @@
-# Description: Update the status of a scheduled job in the Service Catalogue
-import os
 from datetime import datetime
-from utilities.discovery import job
-
-log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
+from utilities.job_log_handling import log_debug, log_error, log_info, log_critical, job
 
 def update(services, status):
   sc = services.sc
-  log = services.log
   sc_scheduled_jobs_data = sc.get_record( 'scheduled-jobs', 'name', job.name)
   job_data = {
     "last_scheduled_run": datetime.now().isoformat(),
@@ -22,5 +17,6 @@ def update(services, status):
     sc.update('scheduled-jobs', job_id, job_data)
     return True
   except Exception as e:
-    log.error(f"Job {job.name} not found in Service Catalogue")
+    log_error(f"Job {job.name} not found in Service Catalogue")
     return False
+  
